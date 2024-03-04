10466f97ae1a9eab55422cc67fed0156873d06ea
biuea <biuea3866@gmail.com>
2024-03-05 01:12:59 +0900

    스프링 컨테이너 적용

--- src/main/kotlin/com/biuea/inflearnbasic/AppConfig.kt
+++ src/main/kotlin/com/biuea/inflearnbasic/AppConfig.kt
@@ -1,23 +1,40 @@
 package com.biuea.inflearnbasic
 
+import com.biuea.inflearnbasic.discount.DiscountPolicy
 import com.biuea.inflearnbasic.discount.FixDiscountPolicy
+import com.biuea.inflearnbasic.member.MemberRepository
 import com.biuea.inflearnbasic.member.MemberService
 import com.biuea.inflearnbasic.member.MemberServiceImpl
 import com.biuea.inflearnbasic.member.MemoryMemberRepository
 import com.biuea.inflearnbasic.order.OrderService
 import com.biuea.inflearnbasic.order.OrderServiceImpl
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
 /**
  *  구성과 역할을 분리하기 위한 클래스
  *  client(service)에 대해 어떤 구현 객체가 주입될지를 결정하는 클래스
  *  이로 인해 클라이언트 코드는 구현 객체를 몰라도 되며, 클라이언트의 코드를 변경하지 않고도 구현 객체를 변경할 수 있다. 인터페이스를 의존하기 때문
  */
+@Configuration
 class AppConfig {
+    @Bean
     fun memberService(): MemberService {
-        return MemberServiceImpl(MemoryMemberRepository())
+        return MemberServiceImpl(this.memberRepository())
     }
 
+    @Bean
+    fun memberRepository(): MemberRepository {
+        return MemoryMemberRepository()
+    }
+
+    @Bean
+    fun discountPolicy(): DiscountPolicy {
+        return FixDiscountPolicy()
+    }
+
+    @Bean
     fun orderService(): OrderService {
-        return OrderServiceImpl(MemoryMemberRepository(), FixDiscountPolicy())
+        return OrderServiceImpl(this.memberRepository(), this.discountPolicy())
     }
 }
--- src/main/kotlin/com/biuea/inflearnbasic/OrderApp.kt
+++ src/main/kotlin/com/biuea/inflearnbasic/OrderApp.kt
@@ -5,23 +5,24 @@ import com.biuea.inflearnbasic.member.Member
 import com.biuea.inflearnbasic.member.MemberServiceImpl
 import com.biuea.inflearnbasic.order.OrderService
 import com.biuea.inflearnbasic.order.OrderServiceImpl
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
-class OrderApp {
-    fun main(args: Array<String>) {
-        val appConfig = AppConfig()
-        val memberService = appConfig.memberService()
-        val orderService = appConfig.orderService()
+class OrderApp
 
-        val memberId = 1L
-        val member = Member(
-            id = memberId,
-            name = "memberA",
-            grade = Grade.VIP
-        )
-        memberService.join(member)
-        val order = orderService.createOrder(memberId, "itemA", 10000)
+fun main(args: Array<String>) {
+    val applicationContext = AnnotationConfigApplicationContext(AppConfig::class.java)
+    val memberService = applicationContext.getBean("memberService", MemberServiceImpl::class.java)
+    val orderService = applicationContext.getBean("orderService", OrderServiceImpl::class.java)
 
-        println("order = $order")
-        println("order.calculatePrice = ${order.calculatePrice()}")
-    }
+    val memberId = 1L
+    val member = Member(
+        id = memberId,
+        name = "memberA",
+        grade = Grade.VIP
+    )
+    memberService.join(member)
+    val order = orderService.createOrder(memberId, "itemA", 10000)
+
+    println("order = $order")
+    println("order.calculatePrice = ${order.calculatePrice()}")
 }
