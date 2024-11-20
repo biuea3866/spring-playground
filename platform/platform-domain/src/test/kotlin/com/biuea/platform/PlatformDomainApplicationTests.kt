f60cad55834760d14af933b6ef6ec2a7507560ce
biuea <biuea@doodlin.co.kr>
2024-11-20 22:38:49 +0900

    WIP on main

---
+++ platform/platform-domain/src/test/kotlin/com/biuea/platform/PlatformDomainApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.biuea.platform
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PlatformDomainApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
---
+++ platform/platform-infra/src/test/kotlin/com/biuea/platform/PlatformInfraApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.biuea.platform
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PlatformInfraApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
