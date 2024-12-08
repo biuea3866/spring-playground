47bb70be30cf64c104241102c3c7ee628a1d15d7
biuea <biuea@doodlin.co.kr>
2024-12-08 21:58:16 +0900

    WIP on oauth-server

--- build.gradle.kts
+++ build.gradle.kts
@@ -86,7 +86,9 @@ subprojects {
         implementation("org.springframework.boot:spring-boot-starter-data-jpa")
         implementation("org.springframework.boot:spring-boot-starter-web")
         implementation("org.springframework.boot:spring-boot-starter-security")
+        implementation("org.springframework.boot:spring-boot-starter-security:spring-security-oauth2-resource-server")
         implementation("org.springframework.boot:spring-boot-starter-validation")
+        implementation("org.springframework.boot:spring-boot-starter-oauth2-authorization-server")
         implementation(platform("org.springframework.boot:spring-boot-dependencies:$springBootVersion"))
 
         implementation("org.jetbrains.kotlin:kotlin-reflect")
--- table-reserving/table-reserving-application/src/main/kotlin/com/biuea/tablereservingapplication/config/SecurityConfig.kt
+++ table-reserving/table-reserving-application/src/main/kotlin/com/biuea/tablereservingapplication/config/SecurityConfig.kt
@@ -3,13 +3,19 @@ package com.biuea.tablereservingapplication.config
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.annotation.Order
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache
+import java.util.UUID
 
 @Configuration
 @EnableWebSecurity
@@ -30,7 +36,12 @@ class SecurityConfig(
     }
 
     @Bean
+    @Order(1)
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        val authorizationServerConfigurer = OAuth2AuthorizationServerConfigurer()
+        
+
+
         return http.csrf { it.disable()}
             .authorizeHttpRequests {
                 it.requestMatchers(
@@ -41,5 +52,11 @@ class SecurityConfig(
             }
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
             .build()
+    }
+
+    @Bean
+    fun registeredClientRepository(): RegisteredClientRepository {
+        val registeredClient = RegisteredClient.withId(UUID)
+        return InMemoryRegisteredClientRepository()
     }
 }
