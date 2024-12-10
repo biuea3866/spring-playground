2b38fc0aa0afb2f05ceff37481a8c5a54243c8ba
biuea <biuea@doodlin.co.kr>
2024-12-11 00:12:20 +0900

    gateway app 라우팅 예제 추가

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -27,7 +27,8 @@ include(
     ":table-reserving-admin-api",
     ":table-reserving-api",
     ":table-reserving-application",
-    ":table-reserving-authn-api"
+    ":table-reserving-authn-api",
+    ":table-reserving-gateway"
 //    ":platform-domain",
 //    ":platform-infra"
 )
@@ -36,5 +37,6 @@ project(":table-reserving-admin-api").projectDir = file("table-reserving/table-r
 project(":table-reserving-api").projectDir = file("table-reserving/table-reserving-api")
 project(":table-reserving-application").projectDir = file("table-reserving/table-reserving-application")
 project(":table-reserving-authn-api").projectDir = file("table-reserving/table-reserving-authn-api")
+project(":table-reserving-gateway").projectDir = file("table-reserving/table-reserving-gateway")
 //project(":platform-domain").projectDir = file("platform/platform-domain")
 //project(":platform-infra").projectDir = file("platform/platform-infra")
