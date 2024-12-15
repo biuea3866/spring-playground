b0b24036d700ee563d6ddb123b9457dafc7ba79b
biuea <biuea@doodlin.co.kr>
2024-12-15 18:13:08 +0900

    trace, logging filter 추가

--- table-reserving/table-reserving-gateway/src/main/kotlin/com/biuea/tablereservinggateway/router/Router.kt
+++ table-reserving/table-reserving-gateway/src/main/kotlin/com/biuea/tablereservinggateway/router/Router.kt
@@ -1,7 +1,9 @@
 package com.biuea.tablereservinggateway.router
 
 import com.biuea.tablereservinggateway.filter.AppAuthenticationFilter
+import com.biuea.tablereservinggateway.filter.LoggingFilter
 import com.biuea.tablereservinggateway.filter.PlatformUserAuthorizationFilter
+import com.biuea.tablereservinggateway.filter.TraceFilter
 import com.biuea.tablereservinggateway.service.AuthenticationService
 import org.springframework.cloud.gateway.route.RouteLocator
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
@@ -13,7 +15,9 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 class Router(
     private val appAuthenticationFilter: AppAuthenticationFilter,
-    private val platformUserAuthorizationFilter: PlatformUserAuthorizationFilter
+    private val platformUserAuthorizationFilter: PlatformUserAuthorizationFilter,
+    private val loggingFilter: LoggingFilter,
+    private val traceFilter: TraceFilter
 ) {
     @Bean
     fun routingPattern(builder: RouteLocatorBuilder): RouteLocator {
@@ -26,6 +30,8 @@ class Router(
         builder.route(id = "app-api-routing") {
             path("/app")
                 .filters { f ->
+                    f.filter(loggingFilter.apply(LoggingFilter.Config()))
+                    f.filter(traceFilter.apply(TraceFilter.Config()))
                     f.filter(appAuthenticationFilter.apply(AppAuthenticationFilter.Config()))
                     f.filter(platformUserAuthorizationFilter.apply(PlatformUserAuthorizationFilter.Config()))
                     f.rewritePath("/app", "/api/app")
