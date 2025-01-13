136a7bc553097c8cbd7657e54753913502847865
biuea <biuea3866@gmail.com>
2025-01-13 21:29:13 +0900

    generic을 통한 ocp, lsp 위반 방지

--- object-practice/src/main/kotlin/com/biuea/objectpractice/programmer/Paper.kt
+++ object-practice/src/main/kotlin/com/biuea/objectpractice/programmer/Paper.kt
@@ -1,25 +1,40 @@
 package com.biuea.objectpractice.programmer
 
-interface Paper {
+//interface Paper {
+//    fun setData(programmer: Programmer)
+//}
+
+// generic을 사용하여 다운캐스팅을 방지한다.
+// generic은 추상형을 유지하면서 구상형을 제공한다.
+interface Paper<T: Programmer> {
+    fun setData(programmer: T)
 }
 
+// generic으로 변경하면서 구상형으로 제약되기 때문에 다운캐스팅을 방지하고 ocp, lsp를 지킬 수 있다.
 class Client private constructor(
     val library: Library,
     val language: Language,
-    private var _programmer: Programmer
-): Paper {
-    fun setProgrammer(programmer: Programmer) {
+    private var _programmer: FrontEnd
+): Paper<FrontEnd> {
+    fun setProgrammer(programmer: FrontEnd) {
         _programmer = programmer
+    }
+
+    // Paper에게 책임을 위임했지만 Paper를 상속받는 구현체 클래스에서는 다운캐스팅이 일어나고 OCP가 위반된다.
+    override fun setData(programmer: FrontEnd) {
+        programmer.setLanguage(language)
+        programmer.setLibrary(library)
     }
 }
 
+// 하지만 ServerClient와 같이 n개의 관계를 다루게 되는 케이스로는 Client와 같이 구현할 수 없다.
 class ServerClient(
     private val server: Server,
     private val backendLanguage: Language,
     private val frontendLanguage: Language,
     private var _backendProgrammer: Programmer,
     private var _frontendProgrammer: Programmer
-): Paper {
+): Paper<T> {
     val backendProgrammer: Programmer
         get() = _backendProgrammer
 
@@ -32,5 +47,20 @@ class ServerClient(
 
     fun setBackEndProgrammer(programmer: Programmer) {
         _backendProgrammer = programmer
+    }
+
+    // Paper가 Programmer를 알게 하여 관계를 역전시키면
+    // ServerClient는 Programmer와 n의 관계를 가진다.
+    // 따라서 그 만큼의 경우를 다룰 책임이 생긴다.
+    override fun setData(programmer: Programmer) {
+        when(programmer){
+            is FrontEnd -> {
+                programmer.setLanguage(frontendLanguage)
+            }
+            is BackEnd -> {
+                programmer.setLanguage(backendLanguage)
+                programmer.setServer(server)
+            }
+        }
     }
 }
--- object-practice/src/main/kotlin/com/biuea/objectpractice/programmer/Programmer.kt
+++ object-practice/src/main/kotlin/com/biuea/objectpractice/programmer/Programmer.kt
@@ -1,23 +1,36 @@
 package com.biuea.objectpractice.programmer
 
 // Paper를 기반으로 프로그램을 만들 책임이 있다.
-interface Programmer {
-    fun makeProgram(paper: Paper): Program
+//interface Programmer {
+//    fun makeProgram(paper: Paper): Program
+//}
+
+// 중복이라 판단되는 로직이 존재한다면 추상클래스로 전환하여 hook method를 사용한다.
+abstract class Programmer {
+    fun getProgram(paper: Paper): Program {
+        paper.setData(this)
+        return makeProgram(paper)
+    }
+
+    abstract fun makeProgram(paper: Paper): Program
 }
 
 class FrontEnd private constructor(
     private var language: Language,
     private var library: Library
-) : Programmer {
+) : Programmer() {
     constructor() : this("kotlinJS", "vueJS")
 
     override fun makeProgram(paper: Paper): Program {
         // paper에서 어떤 정보도 얻을 수 없으므로 다운캐스팅을 통해 정보를 얻는다.
         // 리스코프 치환 원칙에 어긋난다.
-        if (paper is Client) {
-            this.library = paper.library
-            this.language = paper.language
-        }
+        // 다운캐스팅은 OCP를 위반한다.
+//        if (paper is Client) {
+//            this.library = paper.library
+//            this.language = paper.language
+//        }
+        // 다운캐스팅하지않고, paper에게 책임을 위임한다.
+        paper.setData(this)
 
         return makeFrontEndProgram()
     }
@@ -25,26 +38,39 @@ class FrontEnd private constructor(
     private fun makeFrontEndProgram(): Program {
         return Program()
     }
+
+    fun setLanguage(language: Language) {
+        this.language = language
+    }
+
+    fun setLibrary(library: Library) {
+        this.library = library
+    }
 }
 
 class BackEnd private constructor(
     private var language: Language,
-    private var library: Library
-) : Programmer {
+    private var server: Server
+) : Programmer() {
     constructor() : this("kotlin", "Spring")
 
     override fun makeProgram(paper: Paper): Program {
         // if를 사용하게 되면 런타임에 분기를 맞기는 것이다.
         // if를 사용하는 클라이언트 쪽으로 분기를 옮기는 것이 좋다.
-        if (paper is Client) {
-            this.library = paper.library
-            this.language = paper.language
-        }
+        paper.setData(this)
 
         return makeBackEndProgram()
     }
 
     private fun makeBackEndProgram(): Program {
         return Program()
+    }
+
+    fun setServer(server: Server) {
+        this.server = server
+    }
+
+    fun setLanguage(language: Language) {
+        this.language = language
     }
 }
